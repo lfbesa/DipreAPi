ecd8fc7c791a844cd5287291c8679289f81d03e4
FELIPE BESA <lfbesa@uc.cl>
2018-11-13 17:25:20 -0300

    Fix email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,8 @@
 class ContactMailer < ApplicationMailer
   default from: 'lfbesa@uc.cl'
 
-  def contact_email
-    @contact = params[:contact]
+  def contact_email(contact)
+    @contact = contact
     puts 1
     mail(to:'lfbesa@uc.cl', subject: @contact.subject, from: "lfbesa@uc.cl")
     puts 2
